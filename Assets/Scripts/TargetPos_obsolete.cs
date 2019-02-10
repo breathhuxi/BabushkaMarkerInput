63656e1690963a8a3f73ba74309bfeb70355e691
Xi Hu <breath.hu.xi@gmail.com>
2019-02-10 22:58:14 +0100

    initial commit

---
+++ Assets/Scripts/TargetPos_obsolete.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TargetPos : DefaultTrackableEventHandler {
+
+	// Public variables
+	public GameObject target; // Image Target object
+	public OSC osc; // OSC script
+
+	// private
+	private bool isDetected;
+
+	// Use this for initialization
+	/*void Start () {
+		
+	}*/
+
+	// Update is called once per frame
+	void Update () {
+
+		print (isDetected);
+		
+		if (isDetected) {
+
+			print (target.name);
+			print (target.transform.position);
+			print (target.transform.rotation);
+
+			// Send OSC Message
+			OscMessage messagePos = new OscMessage();	// Instantiate OSC message object
+			messagePos.address = target.name;	// "target": (isDetected, position.x, position.y, position.z, rotation.x, rotation.y, rotation.z)
+			messagePos.values.Add(true); //might have bugs
+			messagePos.values.Add(target.transform.position.x);				
+			messagePos.values.Add(target.transform.position.y);	
+			messagePos.values.Add(target.transform.position.z);	
+			messagePos.values.Add(target.transform.rotation.x);				
+			messagePos.values.Add(target.transform.rotation.y);	
+			messagePos.values.Add(target.transform.rotation.z);
+			osc.Send(messagePos);
+		}
+
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		base.OnTrackableStateChanged (previousStatus, newStatus);
+
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			isDetected = true;
+		}
+		else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+			newStatus == TrackableBehaviour.Status.NOT_FOUND)
+		{
+			isDetected = false;
+
+			// Send OSC Message
+			OscMessage messagePos = new OscMessage();	// Instantiate OSC message object
+			messagePos.address = target.name;	// "target": (isDetected, position.x, position.y, position.z, rotation.x, rotation.y, rotation.z)
+			messagePos.values.Add(false); //might have bugs
+			osc.Send(messagePos);
+		}
+	}
+}
---
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,156 @@
+/*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+///     A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+	// Public variables
+	public GameObject target; // Image Target object
+	public OSC osc; // OSC script
+
+	// private
+	private bool isDetected;
+
+	#region PRIVATE_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+
+    #endregion // PRIVATE_MEMBER_VARIABLES
+
+    #region UNTIY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+		if (isDetected) {
+
+			print (isDetected);
+
+			print (target.name);
+			print (target.transform.position);
+			print (target.transform.rotation);
+
+			// Send OSC Message
+			OscMessage messagePos = new OscMessage();	// Instantiate OSC message object
+			messagePos.address = mTrackableBehaviour.TrackableName;	// "target": (isDetected, position.x, position.y, position.z, rotation.x, rotation.y, rotation.z, rotation.w)
+			messagePos.values.Add("true"); //might have bugs
+			messagePos.values.Add(target.transform.position.x);				
+			messagePos.values.Add(target.transform.position.y);	
+			messagePos.values.Add(target.transform.position.z);	
+			messagePos.values.Add(target.transform.rotation.x);				
+			messagePos.values.Add(target.transform.rotation.y);	
+			messagePos.values.Add(target.transform.rotation.z);
+			messagePos.values.Add(target.transform.rotation.w);
+			osc.Send(messagePos);
+		}
+
+	}
+
+    #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+
+			isDetected = true;
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+
+			isDetected = false;
+			print (isDetected);
+
+			// Send OSC Message
+			OscMessage messagePos = new OscMessage();	// Instantiate OSC message object
+			//messagePos.address = target.name;	// "target" instance is destroyed
+			messagePos.address = mTrackableBehaviour.TrackableName;
+			messagePos.values.Add("false"); //might have bugs
+			osc.Send(messagePos);
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PRIVATE_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+
+    #endregion // PRIVATE_METHODS
+}
